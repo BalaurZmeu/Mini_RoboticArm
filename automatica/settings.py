7ade0a13774eb62d3b08adc17d569d5995f7e3e9
BalaurZmeu <balaur.zmeu@gmail.com>
2024-09-04 21:04:29 +0300

    Installed psycopg2 for PostgreSQL and changed settings.py

--- automatica/settings.py
+++ automatica/settings.py
@@ -71,13 +71,17 @@ WSGI_APPLICATION = 'automatica.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# Set the postgresql database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DATABASE_NAME'),
+        'USER': config('DATABASE_USER'),
+        'PASSWORD': config('DATABASE_PASSWORD'),
+        'HOST': config('DATABASE_HOST', default='localhost'),
+        'PORT': config('DATABASE_PORT', default='5432'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
