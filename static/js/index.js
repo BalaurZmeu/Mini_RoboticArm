9cb82d48b45f59cc04ebd72a8f9f88e9bae52354
BalaurZmeu <balaur.zmeu@gmail.com>
2024-09-13 14:43:48 +0300

    Added favicon and trying to make JavaScript work

--- static/js/index.js
+++ static/js/index.js
@@ -1,14 +1,51 @@
 console.log('JavaScript file loaded');
 
-function getCSRFToken() {
-  return document.querySelector('input[name="csrfmiddlewaretoken"]').value;
+// Function to GET csrftoken from Cookie
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
 }
+
+var csrftoken = getCookie('csrftoken');
+
+/*****************************************************************************/
+
+function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+
+/*****************************************************************************/
+
+// Function to set Request Header with `CSRFTOKEN`
+function setRequestHeader(){
+    $.ajaxSetup({
+        beforeSend: function(xhr, settings) {
+            if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+                xhr.setRequestHeader("X-CSRFToken", csrftoken);
+            }
+        }
+    });
+}
+
+/*****************************************************************************/
 
 function sendCommand(action) {
   const xhr = new XMLHttpRequest();
   xhr.open('POST', '/robot/', true);
   xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-  xhr.setRequestHeader('X-CSRFToken', getCSRFToken());  // Fetch CSRF token correctly
+  xhr.setRequestHeader('X-CSRFToken', csrftoken);  // Fetch CSRF token correctly
 
   xhr.onreadystatechange = function() {
     if (xhr.readyState === XMLHttpRequest.DONE) {
