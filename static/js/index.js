ea49a1d97413e7575ac60e6784e0070fb3ad2566
BalaurZmeu <balaur.zmeu@gmail.com>
2024-09-13 14:53:20 +0300

    Still working on JS code

--- static/js/index.js
+++ static/js/index.js
@@ -1,12 +1,14 @@
 console.log('JavaScript file loaded');
 
+/*****************************************************************************/
+
 // Function to GET csrftoken from Cookie
 function getCookie(name) {
     var cookieValue = null;
     if (document.cookie && document.cookie !== '') {
         var cookies = document.cookie.split(';');
         for (var i = 0; i < cookies.length; i++) {
-            var cookie = jQuery.trim(cookies[i]);
+            var cookie = cookies[i].trim();  // Use native JavaScript trim
             // Does this cookie string begin with the name we want?
             if (cookie.substring(0, name.length + 1) === (name + '=')) {
                 cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
@@ -24,19 +26,6 @@ var csrftoken = getCookie('csrftoken');
 function csrfSafeMethod(method) {
     // these HTTP methods do not require CSRF protection
     return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
-}
-
-/*****************************************************************************/
-
-// Function to set Request Header with `CSRFTOKEN`
-function setRequestHeader(){
-    $.ajaxSetup({
-        beforeSend: function(xhr, settings) {
-            if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
-                xhr.setRequestHeader("X-CSRFToken", csrftoken);
-            }
-        }
-    });
 }
 
 /*****************************************************************************/
