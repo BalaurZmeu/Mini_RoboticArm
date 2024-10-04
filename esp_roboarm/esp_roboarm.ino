dd5a50a472d7998f9d47be909ab5f68aa3407147
BalaurZmeu <balaur.zmeu@gmail.com>
2024-10-04 17:01:00 +0300

    Changed the ESP sketch to call the function moveServo

--- esp_roboarm/esp_roboarm.ino
+++ esp_roboarm/esp_roboarm.ino
@@ -116,52 +116,52 @@ void loop() {
         "</head><body><h1>Robotic Arm Control</h1>"
         "<div class=\"ctrl-pnl\"><table>"
         "<tr><td><button class=\"ctrl-btn\""
-        "onmousedown=\"sendCommand('rotate_left_on')\" "
-        "onmouseup=\"sendCommand('rotate_left_off')\" "
-        "ontouchstart=\"sendCommand('rotate_left_on')\" "
-        "ontouchend=\"sendCommand('rotate_left_off')\">"
+        "onmousedown=\"sendCommand('rotate_left')\" "
+        "onmouseup=\"sendCommand('stop')\" "
+        "ontouchstart=\"sendCommand('rotate_left')\" "
+        "ontouchend=\"sendCommand('stop')\">"
         "<i class=\"bi bi-arrow-counterclockwise\"></i></button></td>"
         "<td><button class=\"ctrl-btn\""
-        "onmousedown=\"sendCommand('rotate_right_on')\" "
-        "onmouseup=\"sendCommand('rotate_right_off')\" "
-        "ontouchstart=\"sendCommand('rotate_right_on')\" "
-        "ontouchend=\"sendCommand('rotate_right_off')\">"
+        "onmousedown=\"sendCommand('rotate_right')\" "
+        "onmouseup=\"sendCommand('stop')\" "
+        "ontouchstart=\"sendCommand('rotate_right')\" "
+        "ontouchend=\"sendCommand('stop')\">"
         "<i class=\"bi bi-arrow-clockwise\"></i></button></td></tr>"
         "<tr><td><button class=\"ctrl-btn\""
-        "onmousedown=\"sendCommand('shoulder_fwd_on')\" "
-        "onmouseup=\"sendCommand('shoulder_fwd_off')\" "
-        "ontouchstart=\"sendCommand('shoulder_fwd_on')\" "
-        "ontouchend=\"sendCommand('shoulder_fwd_off')\">"
+        "onmousedown=\"sendCommand('shoulder_fwd')\" "
+        "onmouseup=\"sendCommand('stop')\" "
+        "ontouchstart=\"sendCommand('shoulder_fwd')\" "
+        "ontouchend=\"sendCommand('stop')\">"
         "<i class=\"bi bi-arrow-bar-up\"></i></button></td>"
         "<td><button class=\"ctrl-btn\""
-        "onmousedown=\"sendCommand('shoulder_bkd_on')\" "
-        "onmouseup=\"sendCommand('shoulder_bkd_off')\" "
-        "ontouchstart=\"sendCommand('shoulder_bkd_on')\" "
-        "ontouchend=\"sendCommand('shoulder_bkd_off')\">"
+        "onmousedown=\"sendCommand('shoulder_bkd')\" "
+        "onmouseup=\"sendCommand('stop')\" "
+        "ontouchstart=\"sendCommand('shoulder_bkd')\" "
+        "ontouchend=\"sendCommand('stop')\">"
         "<i class=\"bi bi-arrow-bar-down\"></i></button></td></tr>"
         "<tr><td><button class=\"ctrl-btn\""
-        "onmousedown=\"sendCommand('elbow_up_on')\" "
-        "onmouseup=\"sendCommand('elbow_up_off')\" "
-        "ontouchstart=\"sendCommand('elbow_up_on')\" "
-        "ontouchend=\"sendCommand('elbow_up_off')\">"
+        "onmousedown=\"sendCommand('elbow_up')\" "
+        "onmouseup=\"sendCommand('stop')\" "
+        "ontouchstart=\"sendCommand('elbow_up')\" "
+        "ontouchend=\"sendCommand('stop')\">"
         "<i class=\"bi bi-arrow-up-left\"></i></button></td>"
         "<td><button class=\"ctrl-btn\""
-        "onmousedown=\"sendCommand('elbow_down_on')\" "
-        "onmouseup=\"sendCommand('elbow_down_off')\" "
-        "ontouchstart=\"sendCommand('elbow_down_on')\" "
-        "ontouchend=\"sendCommand('elbow_down_off')\">"
+        "onmousedown=\"sendCommand('elbow_down')\" "
+        "onmouseup=\"sendCommand('stop')\" "
+        "ontouchstart=\"sendCommand('elbow_down')\" "
+        "ontouchend=\"sendCommand('stop')\">"
         "<i class=\"bi bi-arrow-down-right\"></i></button></td></tr>"
         "<tr><td><button class=\"ctrl-btn\""
-        "onmousedown=\"sendCommand('open_claw_on')\" "
-        "onmouseup=\"sendCommand('open_claw_off')\" "
-        "ontouchstart=\"sendCommand('open_claw_on')\" "
-        "ontouchend=\"sendCommand('open_claw_off')\">"
+        "onmousedown=\"sendCommand('open_claw')\" "
+        "onmouseup=\"sendCommand('stop')\" "
+        "ontouchstart=\"sendCommand('open_claw')\" "
+        "ontouchend=\"sendCommand('stop')\">"
         "<i class=\"bi bi-arrows\"></i></button></td>"
         "<td><button class=\"ctrl-btn\""
-        "onmousedown=\"sendCommand('close_claw_on')\" "
-        "onmouseup=\"sendCommand('close_claw_off')\" "
-        "ontouchstart=\"sendCommand('close_claw_on')\" "
-        "ontouchend=\"sendCommand('close_claw_off')\">"
+        "onmousedown=\"sendCommand('close_claw')\" "
+        "onmouseup=\"sendCommand('stop')\" "
+        "ontouchstart=\"sendCommand('close_claw')\" "
+        "ontouchend=\"sendCommand('stop')\">"
         "<i class=\"bi bi-arrows-collapse-vertical\"></i></button></td></tr>"
         "</table></div>"
         "<script>"
@@ -177,34 +177,42 @@ void loop() {
 
     if (request.indexOf("/rotate_left") != -1) {
       Serial.println("rotate_left");
+      moveServo(base, "negative");
     }
     
     if (request.indexOf("/rotate_right") != -1) {
       Serial.println("rotate_right");
+      moveServo(base, "positive");
     }
 
     if (request.indexOf("/shoulder_fwd") != -1) {
       Serial.println("shoulder_fwd");
+      moveServo(shoulder, "positive");
     }
 
     if (request.indexOf("/shoulder_bkd") != -1) {
       Serial.println("shoulder_bkd");
+      moveServo(shoulder, "negative");
     }
 
     if (request.indexOf("/elbow_up") != -1) {
       Serial.println("elbow_up");
+      moveServo(elbow, "positive");
     }
 
     if (request.indexOf("/elbow_down") != -1) {
       Serial.println("elbow_down");
+      moveServo(elbow, "negative");
     }
 
     if (request.indexOf("/open_claw") != -1) {
       Serial.println("open_claw");
+      moveServo(claw, "positive");
     }
 
     if (request.indexOf("/close_claw") != -1) {
       Serial.println("close_claw");
+      moveServo(claw, "negative");
     }
   }
 }
